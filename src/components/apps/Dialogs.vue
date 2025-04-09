94f025d13ec6df18b767c44caf33a2c171451409
qhd410 <qhdcks410@naver.com>
2025-04-09 18:21:12 +0900

    고개등록 개발

--- src/components/apps/Dialogs.vue
+++ src/components/apps/Dialogs.vue
@@ -1,44 +1,43 @@
 <template>
   <div class="text-center pa-4">
     <v-dialog
-      v-model="dialog"
-      max-width="400"
-      persistent
-    >
-      <!-- <template v-slot:activator="{ props: activatorProps }">
-        <v-btn v-bind="activatorProps">
-          Open Dialog
-        </v-btn>
-      </template> -->
-
-      <v-card
-        prepend-icon="mdi-map-marker"
-        text="Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running."
-        title="Use Google's location service?"
-      >
+      :model-value="props.modelValue" @update:modelValue="closeDialog" max-width="800" >
+    
+      <template v-slot:default>
+        <v-card title="상담내용">
+        <template v-slot:text>
+          <v-textarea :model-value="props.connValue" label="" rows="20"></v-textarea>
+        </template>
         <template v-slot:actions>
           <v-spacer></v-spacer>
-          <v-btn @click="dialog = false">
+          <v-btn @click="closeDialog(false)">
             확인
           </v-btn>
         </template>
       </v-card>
+    </template> 
     </v-dialog>
   </div>
 </template>
 
 <script setup>
-  import { ref, watch } from 'vue'
+import { defineProps, defineEmits } from 'vue'
 
-  const props = defineProps({
-    isActive: Boolean,
-  })
+// v-model을 사용하기 위해 'modelValue'라는 이름의 prop을 정의합니다.
+const props = defineProps({
+  modelValue: Boolean, // Boolean 타입의 modelValue prop (v-model="상태" 에서 '상태'가 전달됨)
+  connValue: String
+})
 
-  const dialog = ref(props.isActive)
+// v-model을 사용하기 위해 'update:modelValue' 이벤트를 정의합니다.
+const emit = defineEmits(['update:modelValue'])
 
-  // watch(dialog, (newValue) => {
-  //   console.log(111)
-  //   dialog.value = newValue
-  // })
+
+// 다이얼로그를 닫아야 할 때 호출되는 함수
+// 이 함수는 부모 컴포넌트에 'update:modelValue' 이벤트를 발생시켜 상태 변경을 알립니다.
+function closeDialog(value) {
+  // 부모에게 'modelValue'를 'value'(주로 false)로 업데이트하라고 알립니다.
+  emit('update:modelValue', value)
+}
 
 </script>
--- src/views/pages/customer/index.vue
+++ src/views/pages/customer/index.vue
@@ -1,6 +1,6 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { onMounted, reactive, ref } from 'vue';
 import UiParentCard from '@/components/shared/UiParentCard.vue';
 import request from '@/api/request';
 import type { AxiosResponse } from 'axios';
@@ -8,29 +8,108 @@ import Dialogs from '@/components/apps/Dialogs.vue';
 
  const rowData = ref();
  const isActive = ref(false);
- const popValue = ref("sdfsdfdsfsdff");
+ const isRegiter = ref(false);
+ const connValue = ref();
+
+ const search = reactive({
+      valid: false,
+      orderNo: '',
+      ordDate: '',
+      cusNm: '',
+    }
+ )
+
+ const items = [
+    { lable:'카카오', value :  'A'},
+    { lable:'네이버플레이스', value :  'B'},
+    { lable:'직접', value :  'C'}
+  ]
+
+ const saveItem = reactive({
+      orderNo: '',
+      prodNm: '',
+      compYn: '',
+      conDate: '',
+      ordDate: '',
+      cusChnnel: 'A',
+      cusNm: '',
+      cusPhone: '',
+      conn: '',
+      regDate: '',
+      regNm: '',
+      updDate: '',
+      updNm: ''
+    }
+ )
  const headers = ref([
-    { align: 'center', key: 'cusNo', sortable: 'false', title: '주문번호' },
+    { align: 'center', key: 'orderNo', sortable: 'false', title: '주문번호' },
+    { align: "center", key: 'compYn',title:'픽업상태'},
     { align: 'center', key: 'conDate', title: '주문일자' },
-    { align: 'center', key: 'ordData', title: '픽업일자'},
+    { align: 'center', key: 'ordDate', title: '픽업일자'},
     { align: 'center', key: 'cusChnnel', title: '주문채널' },
     { align: 'center', key: 'cusNm', title: '고객명' },
     { align: 'center', key: 'cusPhone', title: '전화번호' },
-    { align: 'center', key: 'conn', title: '상담내용' },
+    { align: 'center', key: 'conn', title: '상담내용', value:"conn" },
     { align: 'center', key: 'regDate', title: '등록일자' },
     { align: 'center', key: 'regNm', title: '등록자' },
     { align: 'center', key: 'updDate', title: '수정일자' },
     { align: 'center', key: 'updNm', title: '수정자' },
+    { align: 'center', title: '버튼' ,value:'comp'},
  ]);
 
  const getOrerList = () => {
+
+    const param = {
+      ...search
+    }
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    request.post('/api/customer/getCustomerList').then((response : AxiosResponse<any, any>) => {
+    request.post('/api/customer/getCustomerList',param).then((response : AxiosResponse<any, any>) => {
       const data = response.data;
       rowData.value = data;
     });
 
-    isActive.value = true
+ }
+
+ const handleClickEvent = (item : any) => {
+  connValue.value = item.conn
+  isActive.value = true
+ }
+
+ const handleCompEvent = (item : any) => {
+
+  const param = {
+    orderNo : item.orderNo,
+    compYn : 'Y'
+  }
+  
+
+  request.post('/api/customer/updateCompYn',param).then((response : AxiosResponse<any, any>) => {
+      if(response.status == 200){
+          alert('처리되었습니다.')
+          getOrerList()
+      }
+    });
+ }
+
+ const showSave = () => {
+  isRegiter.value = true
+ }
+
+ const saveOrder = () => {
+
+  const param = {
+    ...saveItem
+  }  
+
+  request.post('/api/customer/insertCustomer',param).then((response : AxiosResponse<any, any>) => {
+      if(response.status == 200){
+          alert('등록되었습니다.')
+          getOrerList()
+          isRegiter.value = false
+      }
+    });
+
  }
 
  onMounted(async() =>{
@@ -39,13 +118,57 @@ import Dialogs from '@/components/apps/Dialogs.vue';
 
  
 </script>
-
 <template>
   <!-- <BaseBreadcrumb :title="page.title" :breadcrumbs="breadcrumbs"></BaseBreadcrumb> -->
+
+  <v-form>
+    <v-container>
+      <v-row>
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="search.orderNo"
+            label="주문번호"
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+          <v-text-field
+            v-model="search.cusNm"
+            label="고객이름"
+          ></v-text-field>
+        </v-col>
+
+        <v-col
+          cols="12"
+          md="4"
+        >
+        <v-text-field
+            v-model="search.ordDate"
+            label="픽업날짜"
+            type="date"
+          ></v-text-field>
+         
+        <!-- <v-date-input
+          v-model="search.ordData"
+          label="Select range"
+          multiple="range"
+        ></v-date-input> -->
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-form>
+
   <v-row justify="end">
-    <v-col cols="1" md="1">
+    <v-col cols="2" md="2">
+      <v-btn @click="showSave">등록</v-btn>&nbsp;&nbsp;
       <v-btn @click="getOrerList">검색</v-btn>
-    </v-col>
+    </v-col>    
   </v-row>
   <v-row>
     <v-col cols="12" md="12">
@@ -56,14 +179,71 @@ import Dialogs from '@/components/apps/Dialogs.vue';
           items-per-page="10"
           items-per-page-text=""
           filter-mode="every"
-          height="500px"
+          height="500px">
+          <template v-slot:item.conn="{item}">
+            <td>
+              <v-btn @click="handleClickEvent(item)">상세보기</v-btn>
+            </td>
+          </template>      
+          <template v-slot:item.comp="{item}">
+            <td v-if="item.compYn == 'N'">
+              <v-btn @click="handleCompEvent(item)">픽업완료</v-btn>
+            </td>
 
-        ></v-data-table>
+          </template>            
+          
+        </v-data-table>
       </UiParentCard>
     </v-col>
   </v-row>
+  <Dialogs v-model="isActive" :conn-value="connValue" />
+  <v-dialog v-model="isRegiter" max-width="800" >
+    <template v-slot:default>
+      <v-card title="주문등록">
+      <template v-slot:text>
 
-  <Dialogs :is-active="isActive" />
+        <v-select
+          v-model="saveItem.cusChnnel"
+          :items="items"
+          item-title="lable"
+          item-value="value"
+          label="상담채널"
+        ></v-select>
 
-  
+        <v-text-field
+            v-model="saveItem.cusNm"
+            label="고객이름"
+        ></v-text-field>
+
+        <v-text-field
+            v-model="saveItem.cusPhone"
+            label="전화번호"
+        ></v-text-field>        
+
+        <v-text-field
+            v-model="saveItem.ordDate"
+            label="픽업날짜"
+            type="date"
+        ></v-text-field>    
+
+        <v-text-field
+            v-model="saveItem.prodNm"
+            label="상품명"
+        ></v-text-field>  
+        
+
+        <v-textarea v-model="saveItem.conn" label="상담내용" rows="20"></v-textarea>
+      </template>
+      <template v-slot:actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="isRegiter = false">
+          취소
+        </v-btn>            
+        <v-btn @click="saveOrder">
+          확인
+        </v-btn>
+      </template>
+    </v-card>
+  </template> 
+  </v-dialog>
 </template>
